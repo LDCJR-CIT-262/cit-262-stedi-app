9adcdf27ec7a35efbf4b0dcd453211e0573b42ac
diegomayson <zivdiego@gmail.com>
2023-10-18 22:07:49 +0000

    just added

---
+++ screens/helloworld.js
@@ -0,0 +1,6 @@
+const hello =()=>{
+    return ("Hello Diego")
+
+}
+console.log(hello);
+export default hello;
---
+++ screens/helloworld.test.js
@@ -0,0 +1,5 @@
+import hello from "../utils/helloworld.js";
+it ("Should say Hello Diego" ,()=>{
+const helloString = hello;
+expect(helloString).toBe("Hello Diego");
+})
